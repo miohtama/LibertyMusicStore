8e864d2742b324b81bdfe8c6ec72679b8f6172e1
Mikko Ohtamaa <mikko@opensourcehacker.com>
2014-06-29 18:34:58 +0300

    ... And hardening

--- tatianastore/prelisten.py
+++ tatianastore/prelisten.py
@@ -93,7 +93,9 @@ def create_prelisten_on_demand(song):
     outf = os.path.join(settings.MEDIA_ROOT, "prelisten", f)
     mp3 = song.download_mp3.file.name
 
-    assert os.path.exists(mp3), "The MP3 file %s did not exist" % mp3
+    if not os.path.exist(mp3):
+        logger.warn("The MP3 did not exist when tried to start creating prelisten %s", mp3)
+        return
 
     if os.path.exists(outf):
         # Check modification time of the upload versus prelisten
