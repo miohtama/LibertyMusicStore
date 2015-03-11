5a44680b8127c9721e98c931280e96c324cdb544
Mikko Ohtamaa <mikko@opensourcehacker.com>
2015-03-11 13:13:17 -1000

    Adding task to check broken transactions. Some project file updates.

--- tatianastore/tasks.py
+++ tatianastore/tasks.py
@@ -78,3 +78,10 @@ def backup_site():
         # Capture error in Sentry
         logger.error(e.output)
         raise
+
+
+@db_periodic_task(crontab(minute='*/3')):
+def keep_transactions_running():
+    """Keep transactions runnign in the case of notify channel breaking up."""
+    from . import payment
+    payment.scan_open_download_transactions()
