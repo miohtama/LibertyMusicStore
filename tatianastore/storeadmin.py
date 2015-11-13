0363b32912d8b985367f6aa0d5422f79136d29fe
Mikko Ohtamaa <mikko@opensourcehacker.com>
2015-11-13 13:20:15 -0800

    Error attempt 2

--- tatianastore/storeadmin.py
+++ tatianastore/storeadmin.py
@@ -108,7 +108,7 @@ def upload_album(request):
                 logger.exception(e)
                 errors = form._errors.setdefault("zip_upload", ErrorList())
                 errors.append(str(e))
-                raise http.HttpResponseForbidden(str(e))
+                raise HttpResponseForbidden(str(e))
     else:
         logger.info("Not a POST request to upload album")
         form = AlbumUploadForm(request=request)
